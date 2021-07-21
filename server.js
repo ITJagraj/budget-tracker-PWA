0ee884da6f2fe5f9515b37edd666ac2e8f87adac
Jagraj Singh <singh.jagraj96@yahoo.com>
2021-07-20 22:41:52 -0400

    deploying heroku

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ mongoose.connect(MONGODB_URI, {
 mongoose.set('debug', true);
 // routes
 app.use(require("./routes/api.js"));
+
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
